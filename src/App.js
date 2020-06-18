b811844b1ff3431c9a7b807d5d9e9282f45aeea8
qiguanquan <1227655917@qq.com>
2020-06-18 18:27:02 +0800

    基础设置

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
   应用的根组件
 */
 import React, {Component} from 'react'
-
+import { Button, message } from 'antd'
 
 export default class App extends Component {
 
+  handleClick= () => {
+    message.success('This is a message of success')
+  }
+
     render() {
-      return <div>App</div>
+      return <Button type="primary" onClick={this.handleClick}>Primary</Button>
     }
 }
