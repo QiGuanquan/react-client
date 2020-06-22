0af314c4da9748f25b290558aabb18d93c73b45c
qiguanquan <1227655917@qq.com>
2020-06-22 16:53:20 +0800

    主页布局，左边菜单导航

---
+++ src/pages/member-list/member-list.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './member-list.less'
+/**
+ * 通讯录
+ */
+export default class MemberList extends Component {
+  render(){
+    return(
+      <div className='header'>
+        通讯录
+      </div>
+    )
+  }
+}
---
+++ src/pages/task/task.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './task.less'
+/**
+ * 任务
+ */
+export default class Task extends Component {
+  render(){
+    return(
+      <div className='header'>
+        任务
+      </div>
+    )
+  }
+}
